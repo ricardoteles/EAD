54386363485600b8bd9334056e268782aef34bdf
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2025-01-23 16:39:56 -0300

    API Composition Pattern - Creating GET method for user and course

--- authuser/src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
+++ authuser/src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
@@ -1,12 +1,19 @@
 package com.ead.authuser.specifications;
 
+import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Collection;
+import java.util.UUID;
 
 public class SpecificationTemplate {
 
@@ -18,4 +25,12 @@ public class SpecificationTemplate {
             @Spec(path = "fullName", spec = LikeIgnoreCase.class),
     })
     public interface UserSpec extends Specification<UserModel> {}
+
+    public static Specification<UserModel> userCourseId(final UUID courseId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Join<UserModel, UserCourseModel> userJoin = root.join("usersCourses");
+            return cb.equal(userJoin.get("courseId"), courseId);
+        };
+    }
 }
