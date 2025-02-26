00da826165279813a78a2f47590eb18adb142f71
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2025-02-26 19:38:24 -0300

    User's subscription to the course. - Part I

--- course/src/main/java/com/ead/course/clients/AuthUserClient.java
+++ course/src/main/java/com/ead/course/clients/AuthUserClient.java
@@ -2,12 +2,14 @@ package com.ead.course.clients;
 
 import com.ead.course.dtos.ResponsePageDto;
 import com.ead.course.dtos.UserRecordDto;
+import com.ead.course.exceptions.NotFoundException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
@@ -44,5 +46,20 @@ public class AuthUserClient {
             logger.error("Error Request RestClient with cause: {}", e.getMessage());
             throw new RuntimeException("Error Request RestClient", e);
         }
+    }
+
+    public ResponseEntity<UserRecordDto> getOneUserById(UUID userId){
+        String url = baseUrlAuthuser + "/users/" + userId;
+
+        logger.debug("Request URL: {}", url);
+
+        return restClient.get()
+                .uri(url)
+                .retrieve()
+                .onStatus(status -> status.value() == 404, (request, response) -> {
+                    logger.error("Error: User not found: {}", userId);
+                    throw new NotFoundException("Error: User not found.");
+                })
+                .toEntity(UserRecordDto.class);
     }
 }
--- course/src/main/java/com/ead/course/controllers/CourseUserController.java
+++ course/src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -1,7 +1,14 @@
 package com.ead.course.controllers;
 
 import com.ead.course.clients.AuthUserClient;
+import com.ead.course.dtos.SubscriptionRecordDto;
 import com.ead.course.dtos.UserRecordDto;
+import com.ead.course.enums.UserStatus;
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.CourseUserModel;
+import com.ead.course.services.CourseService;
+import com.ead.course.services.CourseUserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,9 +16,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -20,10 +25,36 @@ import java.util.UUID;
 public class CourseUserController {
 
     final private AuthUserClient userClient;
+    final private CourseService courseService;
+    final private CourseUserService courseUserService;
+
     @GetMapping("/courses/{courseId}/users")
     public ResponseEntity<Page<UserRecordDto>> getAllUsersByCourse(
             @PageableDefault(sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
             @PathVariable(value = "courseId") UUID courseId) {
         return ResponseEntity.status(HttpStatus.OK).body(userClient.getAllUsersByCourse(courseId, pageable));
+    }
+
+    @PostMapping("/courses/{courseId}/users/subscription")
+    public ResponseEntity<Object> saveSubscriptionUserInCourse(
+            @PathVariable(value = "courseId") UUID courseId,
+            @RequestBody @Valid SubscriptionRecordDto subscriptionRecordDto) {
+        CourseModel courseModel = courseService.findById(courseId);
+
+        if(courseUserService.existsByCourseAndUserId(courseModel, subscriptionRecordDto.userId())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Subscription already exists!");
+        }
+
+        ResponseEntity<UserRecordDto> responseUser = userClient.getOneUserById(subscriptionRecordDto.userId());
+        if(responseUser.getBody().userStatus().equals(UserStatus.BLOCKED)) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: User is blocked.");
+        }
+
+        CourseUserModel courseUserModel = courseModel.convertToCourseUserModel(subscriptionRecordDto.userId());
+
+        // TODO: send subscription UserInCourse to AuthUser
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(courseUserService.saveAndSendSubscriptionUserInCourse(courseUserModel));
     }
 }
